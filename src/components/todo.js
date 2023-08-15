41b60357543f71d68e0daee46af784510c57acca
beena <beenashrestha61@gmail.com>
2023-08-15 16:39:52 +0545

    todo form ui has been setup

---
+++ src/components/todo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {useDispatch } from 'react-redux';
+import { addTask } from '../actions/taskAction';
+const todoForm = () => {
+  const [task, setTask] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim() !== '') {
+      dispatch(addTask({ id: Date.now(), text: task }));
+      setTask('');
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Add a task'
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type='submit'>Add</button>
+    </form>
+  );
+};
+
+export default todoForm;
